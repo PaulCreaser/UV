0e2021a08f24f3e35eedc622e9faf56f7806d0a5
Paul Creaser <drpaulcreaser@yahoo.co.jp>
2016-01-17 15:03:36 +0900

    Bug fix Pointer to Data

--- BLE_UV.ino
+++ BLE_UV.ino
@@ -1,13 +1,13 @@
 #include <Adafruit_SI1145.h>
 #include <BLE_API.h>
 
-//#define DEBUG 1
+#define DEBUG 1
 
 BLE                                       ble;
 Timeout                                   timeout;
 Ticker                                    uv_ticker;
-Ticker                                    ir_ticker;
-Ticker                                    hi_ticker;
+//Ticker                                    ir_ticker;
+//Ticker                                    hi_ticker;
 
 Adafruit_SI1145 uv_dev = Adafruit_SI1145();
 
@@ -19,9 +19,9 @@ static const uint8_t service1_hi_uuid[]             = {0x71, 0x3D, 0, 2, 0x50, 0
 
 static const uint8_t uart_base_uuid_rev[]           = {0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71};
 
-static uint16_t uv_value[1] = {0};
-static uint16_t ir_value[1] = {0};
-static uint16_t hi_value[1] = {0};
+uint16_t uv_value = 0x10;
+uint16_t ir_value = 0x10;
+uint16_t hi_value = 0x10;
 
 GattCharacteristic  characteristic1(service1_uv_uuid, (uint8_t*)&uv_value, 1, 2, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
 
@@ -34,51 +34,6 @@ GattCharacteristic *sensorChars[] = {&characteristic1, &characteristic2, &charac
 GattService         uartService(service1_uuid, sensorChars, sizeof(sensorChars) / sizeof(GattCharacteristic *));
 
 bool connected = false;
-
-
-void m_uv_ticker_handle(void)
-{
-  if (!connected) return;
-  uv_value[0] = uv_dev.readUV();
-  float UVindex = (float)uv_value[0];
-  UVindex /= 100.0;
-  #ifdef DEBUG
-  Serial.print("UV:");  Serial.println(UVindex);
-  #endif    
-  m_uv_handle();
-}
-
-void m_ir_ticker_handle(void)
-{
-  if (!connected) return;  
-  ir_value[0] = uv_dev.readIR();
-  #ifdef DEBUG
-  Serial.print("IR:"); Serial.print(ir_value[0]);
-  #endif    
-  m_ir_handle();
-}
-
-void m_hi_ticker_handle(void)
-{
-  if (!connected) return;
-  hi_value[0] = uv_dev.readVisible();
-  #ifdef DEBUG
-  Serial.print("Vis, "); Serial.print(hi_value[0]);
-  #endif    
-  m_hi_handle();
-}
-
-void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
-{
-    connected = false;
-    ble.startAdvertising();
-}
-
-void connectionCallBack(const Gap::ConnectionCallbackParams_t *p_conn_param)
-{
-    connected = false;//true;
-    ble.stopAdvertising();
-}
 
 void m_uv_handle()
 {   //update characteristic data
@@ -93,6 +48,68 @@ void m_ir_handle()
 void m_hi_handle()
 {   //update characteristic data
     ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t*)&hi_value, 2);
+}
+
+
+void m_uv_ticker_handle(void)
+{
+  uv_value = uv_dev.readUV();
+  float UVindex = (float)uv_value;
+  UVindex /= 100.0;
+  #ifdef DEBUG
+  Serial.print("UV:");  Serial.println(UVindex);
+  #endif    
+  if (!connected) return;
+  m_uv_handle();
+}
+
+void m_ir_ticker_handle(void)
+{
+  ir_value = uv_dev.readIR();
+  #ifdef DEBUG
+  Serial.print("IR:"); Serial.print(ir_value);
+  #endif    
+  if (!connected) return;  
+  m_ir_handle();
+}
+
+void m_hi_ticker_handle(void)
+{
+  hi_value = uv_dev.readVisible();
+  #ifdef DEBUG
+  Serial.print("Vis:"); Serial.print(hi_value);
+  #endif    
+  if (!connected) return;
+  m_hi_handle();
+}
+
+void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
+{
+    connected = false;
+    ble.startAdvertising();
+}
+
+void connectionCallBack(const Gap::ConnectionCallbackParams_t *p_conn_param)
+{
+    connected = true;
+    ble.stopAdvertising();
+}
+
+
+int v_index =0;
+
+void m_ticker_handle()
+{
+  if (v_index==0) {
+    m_uv_ticker_handle();
+    v_index++;
+  } else if (v_index==1) {
+    m_ir_ticker_handle();
+    v_index++;
+  } else if (v_index==2) {
+    m_hi_ticker_handle();
+    v_index=0;
+  }
 }
 
 void setup() {
@@ -118,7 +135,7 @@ void setup() {
     // add service
     ble.addService(uartService);
     // set device name
-    ble.setDeviceName((const uint8_t *)"PAC_UV");
+    ble.setDeviceName((const uint8_t *)"UV");
     // set tx power,valid values are -40, -20, -16, -12, -8, -4, 0, 4
     ble.setTxPower(4);
     // set adv_interval, 100ms in multiples of 0.625ms.
@@ -133,9 +150,7 @@ void setup() {
       Serial.println("Didn't find Si1145");
       #endif    
     }
-    uv_ticker.attach_us(m_uv_ticker_handle, 1000000);
-    hi_ticker.attach_us(m_hi_ticker_handle, 1000000);
-    ir_ticker.attach_us(m_ir_ticker_handle, 1000000);
+    uv_ticker.attach_us(m_ticker_handle,    333333);
 
 }
 
