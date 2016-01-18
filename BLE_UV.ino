1dce37ee7d4508bb387939793c797a072a7d04cf
Paul Creaser <drpaulcreaser@yahoo.co.jp>
2016-01-18 19:53:16 +0900

    Reduce to one characteristic for all data

--- BLE_UV.ino
+++ BLE_UV.ino
@@ -6,81 +6,54 @@
 BLE                                       ble;
 Timeout                                   timeout;
 Ticker                                    uv_ticker;
-//Ticker                                    ir_ticker;
-//Ticker                                    hi_ticker;
 
 Adafruit_SI1145 uv_dev = Adafruit_SI1145();
 
 // The Nordic UART Service
 static const uint8_t service1_uuid[]                = {0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
 static const uint8_t service1_uv_uuid[]             = {0x71, 0x3D, 0, 4, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
-static const uint8_t service1_ir_uuid[]             = {0x71, 0x3D, 0, 3, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
-static const uint8_t service1_hi_uuid[]             = {0x71, 0x3D, 0, 2, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
 
-static const uint8_t uart_base_uuid_rev[]           = {0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71};
+static const uint8_t uv_base_uuid_rev[]             = {0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71};
 
-uint16_t uv_value = 0x10;
-uint16_t ir_value = 0x10;
-uint16_t hi_value = 0x10;
+uint16_t uv_value[3] = {0,0,0};
 
-GattCharacteristic  characteristic1(service1_uv_uuid, (uint8_t*)&uv_value, 1, 2, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
+GattCharacteristic  characteristic1(service1_uv_uuid, (uint8_t*)uv_value, 1, 6, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
 
-GattCharacteristic  characteristic2(service1_ir_uuid, (uint8_t*)&ir_value, 1, 2, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
+GattCharacteristic *sensorChars[] = {&characteristic1};
 
-GattCharacteristic  characteristic3(service1_hi_uuid, (uint8_t*)&hi_value, 1, 2, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
-
-GattCharacteristic *sensorChars[] = {&characteristic1, &characteristic2, &characteristic3};
-
-GattService         uartService(service1_uuid, sensorChars, sizeof(sensorChars) / sizeof(GattCharacteristic *));
+GattService         uvService(service1_uuid, sensorChars, sizeof(sensorChars) / sizeof(GattCharacteristic *));
 
 bool connected = false;
 
 void m_uv_handle()
 {   //update characteristic data
-    ble.updateCharacteristicValue(characteristic1.getValueAttribute().getHandle(), (uint8_t*)&uv_value, 2);
+    ble.updateCharacteristicValue(characteristic1.getValueAttribute().getHandle(), (uint8_t*)uv_value, 6);
 }
-
-void m_ir_handle()
-{   //update characteristic data
-    ble.updateCharacteristicValue(characteristic2.getValueAttribute().getHandle(), (uint8_t*)&ir_value, 2);
-}
-
-void m_hi_handle()
-{   //update characteristic data
-    ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t*)&hi_value, 2);
-}
-
 
 void m_uv_ticker_handle(void)
 {
-  uv_value = uv_dev.readUV();
-  float UVindex = (float)uv_value;
+  uv_value[0] = uv_dev.readUV();
+  float UVindex = (float)uv_value[0];
   UVindex /= 100.0;
   #ifdef DEBUG
   Serial.print("UV:");  Serial.println(UVindex);
   #endif    
-  if (!connected) return;
-  m_uv_handle();
 }
 
 void m_ir_ticker_handle(void)
 {
-  ir_value = uv_dev.readIR();
+  uv_value[1] = uv_dev.readIR();
   #ifdef DEBUG
-  Serial.print("IR:"); Serial.print(ir_value);
+  Serial.print("IR:"); Serial.print(uv_value[1]);
   #endif    
-  if (!connected) return;  
-  m_ir_handle();
 }
 
 void m_hi_ticker_handle(void)
 {
-  hi_value = uv_dev.readVisible();
+  uv_value[2] = uv_dev.readVisible();
   #ifdef DEBUG
-  Serial.print("Vis:"); Serial.print(hi_value);
+  Serial.print("Vis:"); Serial.print(uv_value[2]);
   #endif    
-  if (!connected) return;
-  m_hi_handle();
 }
 
 void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
@@ -108,6 +81,8 @@ void m_ticker_handle()
     v_index++;
   } else if (v_index==2) {
     m_hi_ticker_handle();
+    if (!connected) return;
+    m_uv_handle();
     v_index=0;
   }
 }
@@ -128,12 +103,12 @@ void setup() {
     ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME,
                                      (const uint8_t *)"UV", sizeof("UV") - 1);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
-                                     (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));
+                                     (const uint8_t *)uv_base_uuid_rev, sizeof(uv_base_uuid_rev));
 
     // set adv_type
     ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
     // add service
-    ble.addService(uartService);
+    ble.addService(uvService);
     // set device name
     ble.setDeviceName((const uint8_t *)"UV");
     // set tx power,valid values are -40, -20, -16, -12, -8, -4, 0, 4
